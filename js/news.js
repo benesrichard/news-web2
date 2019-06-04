67299235ece3ee70357e1973b002b74015bd86b5
Lukáš Pokorný <pokornyl@median.cz>
2019-06-04 13:05:45 +0200

    Preloading

--- js/news.js
+++ js/news.js
@@ -1,30 +1,5 @@
 $(document).ready(function () {
 
-    // Načtení dat z url
-    function getJsonFromUrl(e) {
-        e || (e = location.href);
-        var t = e.indexOf("?"),
-            n = e.indexOf("#");
-        if (-1 == n && -1 == t) return {}; - 1 == n && (n = e.length);
-        var o = -1 == t || n == t + 1 ? e.substring(n) : e.substring(t + 1, n),
-            d = {};
-        return o.split("&").forEach(function (e) {
-            if (e) {
-                var t = (e = e.split("+").join(" ")).indexOf("="),
-                    n = -1 < t ? e.substr(0, t) : e,
-                    o = -1 < t ? decodeURIComponent(e.substr(t + 1)) : "",
-                    l = n.indexOf("[");
-                if (-1 == l) d[decodeURIComponent(n)] = o;
-                else {
-                    var s = n.indexOf("]", l),
-                        r = decodeURIComponent(n.substring(l + 1, s));
-                    n = decodeURIComponent(n.substring(0, l)), d[n] || (d[n] = []), r ? d[n][r] = o : d[n].push(o)
-                }
-            }
-        }), d
-    }
-    
-
     // Generování feature listu
     $("h2").each(function (index) {
         // Přidám id ke každému h2
@@ -81,4 +56,7 @@ $(document).ready(function () {
     }
 
     $('.scrollbar-inner').scrollbar();
+
+    $("#loading").css("display", "none");
+    $("#main-content").css("display", "block");
 });    
---
+++ js/preload.js
@@ -0,0 +1,40 @@
+// Načtení dat z url
+function getJsonFromUrl(e) {
+    e || (e = location.href);
+    var t = e.indexOf("?"),
+        n = e.indexOf("#");
+    if (-1 == n && -1 == t) return {}; - 1 == n && (n = e.length);
+    var o = -1 == t || n == t + 1 ? e.substring(n) : e.substring(t + 1, n),
+        d = {};
+    return o.split("&").forEach(function (e) {
+        if (e) {
+            var t = (e = e.split("+").join(" ")).indexOf("="),
+                n = -1 < t ? e.substr(0, t) : e,
+                o = -1 < t ? decodeURIComponent(e.substr(t + 1)) : "",
+                l = n.indexOf("[");
+            if (-1 == l) d[decodeURIComponent(n)] = o;
+            else {
+                var s = n.indexOf("]", l),
+                    r = decodeURIComponent(n.substring(l + 1, s));
+                n = decodeURIComponent(n.substring(0, l)), d[n] || (d[n] = []), r ? d[n][r] = o : d[n].push(o)
+            }
+        }
+    }), d
+}
+
+urlParams = getJsonFromUrl(location.search);
+
+if(urlParams.theme == "dark" || urlParams.theme == "light")
+{
+    document.getElementById("loading").style.display = "block";
+    document.getElementById("main-content").style.display = "none";
+    if(urlParams.theme == "dark")
+    {
+        document.getElementById("dark-css").rel = "stylesheet";
+    }
+}
+else
+{
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
+}
